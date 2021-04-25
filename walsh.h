fe68f29c7dac1a3ec3b17403d7fef962c5ac331d
Vgeorgee <cracken1111@gmail.com>
2021-04-25 23:29:56 +0200

    feat: add feat vector generator

--- walsh.h
+++ walsh.h
@@ -20,3 +20,13 @@ double walsh_transform(unsigned char *target, int u, int v){
     }
     return sum;
 }
+
+
+void generate_feat_vectors(double *feat_vectors, unsigned char *target, int uv){
+    int number_of_vectors = 0;
+    for(int i = 0; i < uv; i++) {
+        for(int j = 0; j < uv; j++) {
+            feat_vectors[number_of_vectors++] = walsh_transform(target, i, j);
+        }
+    }
+}
